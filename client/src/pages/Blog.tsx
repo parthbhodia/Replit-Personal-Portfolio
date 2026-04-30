da9c78258f6ee9292515f5f25aca410cbd9bde2b
Parth Bhodia <parthbhodia13@gmail.com>
2026-04-30 14:34:17 -0400

    fix(blog): correct Blog JSX structure for TOC layout

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -531,6 +531,7 @@ export default function Blog({ slug }: BlogProps = {}) {
                   url={window.location.href}
                   className="px-4 py-2 rounded-full bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors"
                 />
+              </div>
             </div>
             </div>
 
